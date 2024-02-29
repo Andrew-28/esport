fc9b1107f1e81bb0ab0b24b111028a76ee2fc62d
Ivarovskiy <ivarovskiy00@gmail.com>
2024-02-29 15:13:37 +0200

    fixed autocomplete 2.0

--- src/components/Autocomplete/AutocompleteCP.jsx
+++ src/components/Autocomplete/AutocompleteCP.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import styles from "./Autocomplete.module.css";
+import "./Autocomplete.css";
+
 
 function transformData(data) {
   const transformedData = [];
@@ -23,7 +24,7 @@ const AutocompleteCP = ({ data }) => {
   const arr = transformData(data);
   return (
     <Autocomplete
-      className={styles.autocomplete}
+      className="autocomplete"
       disablePortal
       id="combo-box-demo"
       options={arr}
@@ -31,9 +32,12 @@ const AutocompleteCP = ({ data }) => {
       renderInput={(params) => (
         <TextField
           {...params}
-          hiddenLabel
-          margin="dense"
+          // hiddenLabel
           label="Обрати вид спорту"
+          margin="dense"
+          variant="outlined"
+          sx={{ color: "white" }}
+          InputLabelProps={{ className: "textfieldLabel" }}
         />
       )}
     />
